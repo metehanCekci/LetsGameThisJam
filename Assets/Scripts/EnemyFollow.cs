095d2ec168b01fa811d167e101e6780146c54472
metehanCekci <metehancekci9@gmail.com>
2025-04-06 14:12:10 +0300

    knight

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,12 +5,15 @@ public class EnemyAI : MonoBehaviour
     public float moveSpeed = 2f;
     private Transform player;
     private Rigidbody2D rb;
+    private EnemyMeleeAttack meleeAttack;
 
     private bool isStunned = false;
     private float stunDuration = 0.5f;
 
     void Start()
     {
+        meleeAttack = GetComponent<EnemyMeleeAttack>();
+
         rb = GetComponent<Rigidbody2D>();
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
@@ -21,12 +24,20 @@ public class EnemyAI : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (player != null && !isStunned)
+        if (player != null && !isStunned && (meleeAttack == null || !meleeAttack.isAttacking))
         {
             Vector2 direction = (player.position - transform.position).normalized;
+            float characterFacingX = player.localScale.x;
+            float enemyApproachX = direction.x;
+
+            bool isApproachingFromBehind = (characterFacingX > 0 && enemyApproachX < 0) || (characterFacingX < 0 && enemyApproachX > 0);
+
+            gameObject.GetComponent<SpriteRenderer>().flipX = !isApproachingFromBehind;
+
             rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
         }
     }
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
